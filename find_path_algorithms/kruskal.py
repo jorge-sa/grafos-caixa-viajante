9e7eb25300af3f682a36259c6b56818137923fa3
jorge-sa <lucassairmaos@gmail.com>
2024-03-09 13:32:56 -0300

    kruskal algorithm update: cycle checking

--- find_path_algorithms/kruskal.py
+++ find_path_algorithms/kruskal.py
@@ -2,6 +2,9 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import matrix_builder
 
+#gerar matriz
+mtx = matrix_builder.build_graph_matrix()
+
 class edge():
     def __init__(self, v1, v2, w) -> None:
         self.vtx = (v1,v2)
@@ -27,24 +30,70 @@ def formEdges(matrix:list):
 
     return edges
 
-#gerar matriz
-mtx = matrix_builder.build_graph_matrix()
+def isCycleUtil(graph, vertx, parent, visited):
+    print(vertx, parent, visited)
+    if vertx in visited:
+        return False
+    
+    visited.add(vertx)
+
+    for childV in graph[vertx]:
+        print(childV)
+        if childV in visited and childV != parent:
+            print("já visitado", childV)
+            return True
+        
+        if isCycleUtil(graph, childV, vertx, visited):
+            return True
+    else:
+        return False
+
+def isCycle(v:list, graph:list):
+    adj = {}
+
+    for ve in v:
+        adj[ve] = []
+        for e in graph:
+            if ve in e.vtx:
+                adj[ve].append(e.vtx[0] if e.vtx[0] != ve else e.vtx[1])
+
+    start = list(adj.keys())[0]
+    visited = set()
+    if isCycleUtil(adj, start, -1, visited):
+        print("Found cycle")
+        return True
+    
+    print("no cycle")
+    return False
 
 edges = formEdges(mtx)
 edges.sort(key=lambda x: x.w)
 
 v = []
 arestas = []
-while True:
-    for e in edges:
-        if (e.vtx[1] not in v or e.vtx[0] not in v):
+i = 0
+while len(arestas) < len(mtx)-1:
+    e = edges[i]
+    if (e.vtx[1] not in v or e.vtx[0] not in v):
+        arestas.append(e)
+        v.append(e.vtx[0])
+        v.append(e.vtx[1])
+    else:
+        print("pontos já encontrados")
+        if not isCycle(v, arestas+[e]):
+            print("a added")
             arestas.append(e)
-            v.append(e.vtx[0])
-            v.append(e.vtx[1])
-        if set(v) == {0,1,2,3,4}:
-            break
-    break 
+
+    i += 1
     
 #ver arestas selecionadas
+print("============================")
+print("Arestas selecionadas:")
 for a in arestas:
     print(a.vtx,a.w)
+
+print("============================")
+print("Tamanho do grafo:", len(mtx))
+print("N° de arestas:", len(arestas))
+print("Vértices visitados:", set(v))
+print("============================")
